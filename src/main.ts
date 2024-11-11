e0d9bed1a32388fe4096a160bcc1529c59c744d0
playoffthecuff <jmobile@bk.ru>
2024-11-11 22:52:29 +0300

    docs: correct openAPI spec

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'dotenv/config';
+import { SwaggerModule } from '@nestjs/swagger';
+import { readFileSync } from 'fs';
+import { load } from 'js-yaml';
+import { join } from 'path';
 
 const PORT = process.env.PORT;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerDocument = load(
+    readFileSync(join(__dirname, '../doc/api.yaml'), 'utf8'),
+  );
+  SwaggerModule.setup('doc', app, swaggerDocument);
+
   await app.listen(PORT);
 }
 bootstrap();
